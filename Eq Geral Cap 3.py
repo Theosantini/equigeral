3b0f1fa64590fa2904dad704e7178c9054a561d5
Theosantini <theosantunes@gmail.com>
2020-06-04 19:11:10 -0300

    fazendo p1=1 numerário e eliminando uma equação o resultado dá certinho

--- Eq Geral Cap 3.py
+++ Eq Geral Cap 3.py
@@ -58,19 +58,18 @@ pyw = fsolve(eq, pyw0)
 
 
 eq = GEKKO()
-p1 = eq.Var(value=0.8)
 p2 = eq.Var(value=0.8)
 y1 = eq.Var(value=0.8)
 y2 = eq.Var(value=0.8)
 w1 = eq.Var(value=0.8)
 w2 = eq.Var(value=0.8)
-dem_consum_tot = beta[0, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / p1 + \
-            beta[0, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / p1 + \
+dem_consum_tot = beta[0, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / 1 + \
+            beta[0, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / 1 + \
             beta[1, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / p2 + \
             beta[1, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / p2
-eq.Obj(-1*(dem_consum_tot))
-eq.Equation(beta[0, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / p1 + \
-            beta[0, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / p1 + \
+eq.Obj(-1*(y1 +y2))
+eq.Equation(beta[0, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / 1 + \
+            beta[0, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / 1 + \
             A[0, 0] * y1 + A[0, 1] * y2 - y1 == 0)
 eq.Equation(beta[1, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / p2 + \
             beta[1, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / p2 + \
@@ -79,9 +78,9 @@ eq.Equation((alfa[0, 0] * (w2 / w1) ** alfa[1, 0]) * y1 * va[0] + (alfa[0, 1] * 
             dotacao[0, 0] - dotacao[0, 1] == 0)
 eq.Equation((alfa[1, 0] * (w1 / w2) ** alfa[0, 0]) * y1 * va[0] + (alfa[1, 1] * (w1 / w2) ** alfa[0, 1]) * y2 * va[1] - \
             dotacao[1, 0] - dotacao[1, 1] == 0)
-eq.Equation(p1 - (w1 ** alfa[0, 0]) * (w2 ** alfa[1, 0]) * va[0] - p1 * A[0, 0] - p2 * A[1, 0] == 0)
-eq.Equation(p2 - (w1 ** alfa[0, 1]) * (w2 ** alfa[1, 1]) * va[1] - p1 * A[0, 1] - p2 * A[1, 1] == 0)
+eq.Equation(1 - (w1 ** alfa[0, 0]) * (w2 ** alfa[1, 0]) * va[0] - 1 * A[0, 0] - p2 * A[1, 0] == 0)
+
 eq.solve()
-print(p1.value, p2.value, y1.value, y1.value, w1.value, w1.value)
+print(p2.value, y1.value, y1.value, w1.value, w1.value)
 
 
