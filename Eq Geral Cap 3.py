cd83cf8a32b7966b2dc2ec17e0db0a7f0f21c9c0
Theosantini <theosantunes@gmail.com>
2020-06-04 17:30:35 -0300

    new

--- Eq Geral Cap 3.py
+++ Eq Geral Cap 3.py
@@ -1,8 +1,7 @@
 # Simular modelo EGC CAP 3
 from numpy import *
-import scipy
-import scipy.optimize as opt
 from scipy.optimize import *
+from gekko import GEKKO
 
 i = 1
 
@@ -50,37 +49,33 @@ def eq(pyw):
     return eqc
 
 
-pyw0 = array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+pyw0 = array([0.8, 0.8, 10, 10, 0.8, 0.8])
 
 pyw = fsolve(eq, pyw0)
 
-print(pyw)
+#print(pyw)
+
+eq = GEKKO()
+p1 = eq.Var(value=2)
+p2 = eq.Var(value=2)
+y1 = eq.Var(value=2)
+y2 = eq.Var(value=2)
+w1 = eq.Var(value=2)
+w2 = eq.Var(value=2)
+eq.Obj(-y1 - y2)
+eq.Equation(beta[0, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / p1 + \
+            beta[0, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / p1 + \
+            A[0, 0] * y1 + A[0, 1] * y2 - y1 == 0)
+eq.Equation(beta[1, 0] * (w1 * dotacao[0, 0] + w2 * dotacao[1, 0]) / p2 + \
+            beta[1, 1] * (w1 * dotacao[0, 1] + w2 * dotacao[1, 1]) / p2 + \
+            A[1, 0] * y1 + A[1, 1] * y2 - y2 == 0)
+eq.Equation((alfa[0, 0] * (w2 / w1) ** alfa[1, 0]) * y1 * va[0] + (alfa[0, 1] * (w2 / w1) ** alfa[1, 1]) * y2 * va[1] - \
+            dotacao[0, 0] - dotacao[0, 1] == 0)
+eq.Equation((alfa[1, 0] * (w1 / w2) ** alfa[0, 0]) * y1 * va[0] + (alfa[1, 1] * (w1 / w2) ** alfa[0, 1]) * y2 * va[1] - \
+            dotacao[1, 0] - dotacao[1, 1] == 0)
+eq.Equation(p1 - (w1 ** alfa[0, 0]) * (w2 ** alfa[1, 0]) * va[0] - p1 * A[0, 0] - p2 * A[1, 0] == 0)
+eq.Equation(p2 - (w1 ** alfa[0, 1]) * (w2 ** alfa[1, 1]) * va[1] - p1 * A[0, 1] - p2 * A[1, 1] == 0)
+eq.solve()
+print(p1.value, p2.value, y1.value, y1.value, w1.value, w1.value)
 
 
-
-# Exemplo otimização 1
-
-# objective_fcn = lambda x: x[0]**2 + x[0]*x[1]
-
-# constraint = [{'type':'eq', 'fun' : lambda x: x[0]**3 + x[0]*x[1] - 100},
-#              {'type':'ineq','fun' : lambda x: x[0]**2 + x[1] - 50}]
-
-# x0 = [1, 1]
-
-# bounds = [(-100,100),(-100,100)]
-
-# result = scipy.optimize.minimize(objective_fcn, x0, method = 'SLSQP',
-#                                bounds=bounds, constraints = constraint)
-
-## Exemplo 2: max u((x1**aplpha)*(x2**(1-alpha))) sa p1x1 + p2x2 <= 1, x1>=0, x2>=0
-
-# def utility(x, p, alpha):
-#   return -1.0 * (x[0]**alpha)*(x[1]**(1-alpha))
-
-# def utility_constraints(x, p, alpha):
-#   return np.array([x[0], x[1], 1- p[0]*x[0] - p[1]*x[1]])
-
-# p = np.array([1.0,1.0])
-# alpha = 1.0/3
-# x0 = np.array([.4,.4])
-# opt.fmin_slsqp(utility, x0, f_ieqcons=utility_constraints, args=(p, alpha))
