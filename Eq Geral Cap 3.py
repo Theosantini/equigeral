02cc0c5bfa816121a503f0447d2c4c3d2fdc068b
JoaoMariaOliveira <joao.oliveira@ipea.gov.br>
2020-06-08 22:21:36 -0300

    Rodando o modelo que permite modificar o numero de produtos

--- Eq Geral Cap 3.py
+++ Eq Geral Cap 3.py
@@ -1,66 +1,85 @@
 # Simular modelo EGC CAP 3
 import numpy as np
+import sys
 from numpy.core._multiarray_umath import ndarray
 from scipy.optimize import *
 
-i = 1
+nNumeroBens = 2
 
-k = 1
+nNumeroFatores = 2
 
-h = 1
+nNumeroDomicilios = 1
 
-j = i
+j = nNumeroBens
+nNumeroVariaveis=6
 
-dot = np.array([[30, 20], [20, 5]])
+mDotacao = np.array([[30, 20], [20, 5]])
 
-beta = np.array([[0.3, 0.6], [0.7, 0.4]])
+mBeta = np.array([[0.3, 0.6], [0.7, 0.4]])
 
-A = np.array([[0.2, 0.5], [0.3, 0.25]])
+mCoefTecnicos = np.array([[0.2, 0.5], [0.3, 0.25]])
 
-alfa = np.array([[0.8, 0.4], [0.2, 0.6]])
+mAlfa = np.array([[0.8, 0.4], [0.2, 0.6]])
 
-va = np.array([0.5, 0.25])
+vVa = np.array([0.5, 0.25])
+
+def Modelo(vValores):
+    vPrecosBens= vValores[0:nNumeroBens]
+    vProducao = vValores[nNumeroBens:(nNumeroBens*2)]
+    vPrecosFatores = vValores[(nNumeroBens*2):]
+
+    mEquacoes = np.empty(nNumeroVariaveis)
+    vApoio = np.dot(vPrecosFatores, mDotacao)
+    for n in range(nNumeroBens):
+        vApoio2 = np.dot(mBeta[n,:],vApoio.T)
+        vApoio3 = vApoio2 / vPrecosBens[n]
+        vApoio4 = np.dot(mCoefTecnicos[n,:],vProducao.T)
+        vApoio5 = vApoio3 + vApoio4 - vProducao[n]
+        mEquacoes[n] = vApoio5
+
+    for k in range(nNumeroFatores):
+        vAux=np.zeros(nNumeroBens, dtype=float)
+        vApoio2 = np.sum(mDotacao[k,:])
+        nDivisao= vPrecosFatores[0]/vPrecosFatores[1]
+        if k ==1:
+            nDivisao= 1/nDivisao
+        for p in range(nNumeroBens):
+            vApoio1= vProducao[p] * vVa[p]
+            if k==0:
+                vApoio3 = mAlfa[0,p] * nDivisao **(mAlfa[1,p])
+            else:
+                vApoio3 = mAlfa[1, p] * nDivisao ** (mAlfa[0, p])
+
+            vAux[p] = vApoio3 * vApoio1
+
+        mEquacoes[nNumeroBens+k] = np.sum(vAux) - vApoio2
+
+    for n in range(nNumeroBens):
+        nAux = 1
+        for k in range(nNumeroFatores):
+            x = vPrecosFatores[k] ** mAlfa[k,n]
+            nAux = nAux * vPrecosFatores[k] ** mAlfa[k,n]
+
+        nAux2 = 0
+        for l in range(nNumeroBens):
+            nAux2 = nAux2 + vPrecosBens[l]*mCoefTecnicos[l, n]
+
+        mEquacoes[(nNumeroBens+nNumeroFatores)+n] = vPrecosBens[n] - nAux * vVa[n] - nAux2
+
+    return mEquacoes
+
+if __name__ == '__main__':
+    vValoresIniciais = np.array([1, 0.8, 2, 2, 0.8, 0.8])
+    vResult = fsolve(Modelo, vValoresIniciais)
+    vPrecosBens= vResult[0:nNumeroBens]
+    print("Preços dos Bens", vPrecosBens )
+    vProducao = vResult[nNumeroBens:(nNumeroBens*2)]
+    print("Produção", vProducao )
+    vPrecosFatores = vResult[(nNumeroBens*2):]
+    print("Preços dos Fatores", vPrecosFatores )
+    print("End")
+    sys.exit(0)
 
 
-def eq(pyw, *p1):
-    p1 = numeraire
-    p2 = pyw[0]
-    y1 = pyw[1]
-    y2 = pyw[2]
-    w1 = pyw[3]
-    w2 = pyw[4]
-
-    eqc = np.empty((5))
-
-    eqc[0] = beta[0, 0] * (w1 * dot[0, 0] + w2 * dot[1, 0]) / p1 + \
-             beta[0, 1] * (w1 * dot[0, 1] + w2 * dot[1, 1]) / p1 + \
-             A[0, 0] * y1 + A[0, 1] * y2 - y1
-    eqc[1] = beta[1, 0] * (w1 * dot[0, 0] + w2 * dot[1, 0]) / p2 + \
-             beta[1, 1] * (w1 * dot[0, 1] + w2 * dot[1, 1]) / p2 + \
-             A[1, 0] * y1 + A[1, 1] * y2 - y2
-    eqc[2] = (alfa[0, 0] * (w2 / w1) ** alfa[1, 0]) * y1 * va[0] + (alfa[0, 1] * (w2 / w1) ** alfa[1, 1]) * y2 * va[1] - \
-             dot[0, 0] - dot[0, 1]
-    eqc[3] = (alfa[1, 0] * (w1 / w2) ** alfa[0, 0]) * y1 * va[0] + (alfa[1, 1] * (w1 / w2) ** alfa[0, 1]) * y2 * va[1] - \
-             dot[1, 0] - dot[1, 1]
-    eqc[4] = p1 - (w1 ** alfa[0, 0]) * (w2 ** alfa[1, 0]) * va[0] - p1 * A[0, 0] - p2 * A[1, 0]
-
-    return eqc
-
-
-numeraire = (1)
-
-pyw0 = np.array([0.8, 2, 2, 0.8, 0.8])
-
-pyw = fsolve(eq, pyw0)
-
-# print(pyw)
-
-
-r = []
-for k in range(len(dot)):
-    for h in range(len(dot[0])):
-        for k in range(len(beta)):
-            for h in range(len(beta[0])):
-                r = beta[i][h] * sum((dot[k][h]))
 
 
